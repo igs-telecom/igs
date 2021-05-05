4dc0d6c64f1e4971ac0132755d7ccd354a5b7861
Ibrahim Islam <83713851+igs-telecom@users.noreply.github.com>
2021-05-05 20:29:28 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+$students = array(
+"Limon"
+"ripa"
+"maimun"
+"ambia"
+"jannat"
+"jamil"
+"malek"
+
+);
+
+$ = count ($students);
+for( $i=0; $i <$a ; $i++){
+	echo $students[$i]."<br>";
+}
+
+?>
